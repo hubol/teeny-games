5f5fd1e41dad9c573f433178f971a43b220d0de0
Hubol <hubol.gordon@gmail.com>
2026-01-31 11:45:14 -0600

    library book

--- src/assets/generated/textures/generated-texture-data.ts
+++ src/assets/generated/textures/generated-texture-data.ts
@@ -1,6 +1,6 @@
 // This file is generated
 
-const atlases = [{ url: require("./atlas0.png"), texturesCount: 9 }];
+const atlases = [{ url: require("./atlas0.png"), texturesCount: 10 }];
 
 interface TxData {
   id: string;
@@ -17,14 +17,15 @@ function txs<T>(tx: (data: TxData) => T) {
       Displacement: tx({ id: "Effects.Displacement", atlas: 0, x: 505, y: 0, width: 256, height: 256 }),
     },
     Font: {
-      Diggit: tx({ id: "Font.Diggit", atlas: 0, x: 762, y: 246, width: 128, height: 8 }),
-      ErotixLight: tx({ id: "Font.ErotixLight", atlas: 0, x: 762, y: 176, width: 160, height: 34 }),
-      Erotix: tx({ id: "Font.Erotix", atlas: 0, x: 762, y: 211, width: 160, height: 34 }),
-      Flaccid: tx({ id: "Font.Flaccid", atlas: 0, x: 891, y: 246, width: 102, height: 24 }),
+      Diggit: tx({ id: "Font.Diggit", atlas: 0, x: 501, y: 257, width: 128, height: 8 }),
+      ErotixLight: tx({ id: "Font.ErotixLight", atlas: 0, x: 762, y: 209, width: 160, height: 34 }),
+      Erotix: tx({ id: "Font.Erotix", atlas: 0, x: 762, y: 244, width: 160, height: 34 }),
+      Flaccid: tx({ id: "Font.Flaccid", atlas: 0, x: 630, y: 257, width: 102, height: 24 }),
       GoodBoy: tx({ id: "Font.GoodBoy", atlas: 0, x: 762, y: 0, width: 256, height: 128 }),
     },
     Library: {
       BackgroundBarnes: tx({ id: "Library.BackgroundBarnes", atlas: 0, x: 0, y: 107, width: 500, height: 280 }),
+      Book: tx({ id: "Library.Book", atlas: 0, x: 762, y: 176, width: 182, height: 32 }),
       Cart: tx({ id: "Library.Cart", atlas: 0, x: 762, y: 129, width: 228, height: 46 }),
     },
     Lottie: {
---
+++ src/igua/objects/obj-library-book.ts
@@ -0,0 +1,49 @@
+import { Sprite } from "pixi.js";
+import { objText } from "../../assets/fonts";
+import { Tx } from "../../assets/textures";
+import { PseudoRng, Rng } from "../../lib/math/rng";
+import { container } from "../../lib/pixi/container";
+import { MapRgbFilter } from "../../lib/pixi/filters/map-rgb-filter";
+
+const prng = new PseudoRng();
+
+const [txBook, ...txsBookDecoration] = Tx.Library.Book.split({ width: 26 });
+
+export function objLibraryBook() {
+    const seed = Rng.intc(1_000_000, 999_000_000);
+    prng.seed = seed;
+
+    const api = {
+        get seed() {
+            return seed;
+        },
+    };
+
+    const decorationTx = prng.item(txsBookDecoration);
+    const titleObj = objText.XSmallIrregular(
+        createTitle(),
+        { tint: 0x0000ff, maxWidth: 20, align: Rng.choose("left", "right", "center") },
+    )
+        .anchored(0.5, 0.5)
+        .at(13, 16);
+
+    return container(
+        Sprite.from(txBook),
+        Sprite.from(decorationTx),
+        titleObj,
+    )
+        .pivoted(13, 16)
+        .filtered(new MapRgbFilter(prng.color(), prng.color(), prng.color()))
+        .merge({ objLibraryBook: api });
+}
+
+function createTitle() {
+    const question = prng.bool() ? prng.choose("how", "why", "where", "what", "who") : "";
+    const subject = prng.choose("she", "they", "we", "I");
+    const verb = prng.choose("go", "be", "run", "love", "want");
+
+    return [question, subject, verb]
+        .filter(string => string)
+        .map((string, index) => index > 0 ? string : (string.charAt(0).toUpperCase() + string.substring(1)))
+        .join(" ");
+}
