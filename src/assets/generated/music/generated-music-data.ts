7dc060f103b9789b2fe71decbb549098a22c1e8f
Hubol <hubol.gordon@gmail.com>
2026-02-23 14:57:46 -0600

    Silly music

--- src/assets/generated/music/generated-music-data.ts
+++ src/assets/generated/music/generated-music-data.ts
@@ -2,5 +2,6 @@
 // This file is generated
 
 export const GeneratedMusicData = {
+   "Mishang": { ogg: require("./ogg/mishang.ogg") },
    "Placeholder": { ogg: require("./ogg/placeholder.ogg") }
 }
--- src/igua/scenes/scn-main.ts
+++ src/igua/scenes/scn-main.ts
@@ -1,8 +1,11 @@
 import { Graphics } from "pixi.js";
+import { Mzk } from "../../assets/music";
+import { Jukebox } from "../core/igua-audio";
 import { scene } from "../globals";
 import { mxnMishaControlled, objMisha } from "../objects/obj-misha";
 
 export function scnMain() {
+    Jukebox.play(Mzk.Mishang);
     scene.style.backgroundTint = 0x280284;
 
     objMisha()
