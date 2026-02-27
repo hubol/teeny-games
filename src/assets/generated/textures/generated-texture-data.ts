11a489d50f819ac732633404540b17e03c15a6a5
Hubol <hubol.gordon@gmail.com>
2026-02-26 20:28:52 -0600

    Recipe book that opens

--- src/assets/generated/textures/generated-texture-data.ts
+++ src/assets/generated/textures/generated-texture-data.ts
@@ -1,6 +1,6 @@
 // This file is generated
 
-const atlases = [{ url: require("./atlas0.png"), texturesCount: 28 }];
+const atlases = [{ url: require("./atlas0.png"), texturesCount: 31 }];
 
 interface TxData {
   id: string;
@@ -14,42 +14,47 @@ interface TxData {
 function txs<T>(tx: (data: TxData) => T) {
   return {
     Character: {
-      MishaHandOpened: tx({ id: "Character.MishaHandOpened", atlas: 0, x: 436, y: 306, width: 44, height: 50 }),
-      MishaHand: tx({ id: "Character.MishaHand", atlas: 0, x: 481, y: 338, width: 44, height: 50 }),
+      MishaHandOpened: tx({ id: "Character.MishaHandOpened", atlas: 0, x: 971, y: 231, width: 44, height: 50 }),
+      MishaHand: tx({ id: "Character.MishaHand", atlas: 0, x: 436, y: 374, width: 44, height: 50 }),
       Misha: tx({ id: "Character.Misha", atlas: 0, x: 0, y: 281, width: 380, height: 86 }),
     },
     Font: {
-      Diggit: tx({ id: "Font.Diggit", atlas: 0, x: 810, y: 121, width: 128, height: 8 }),
-      ErotixLight: tx({ id: "Font.ErotixLight", atlas: 0, x: 810, y: 0, width: 160, height: 34 }),
-      Erotix: tx({ id: "Font.Erotix", atlas: 0, x: 810, y: 35, width: 160, height: 34 }),
-      Flaccid: tx({ id: "Font.Flaccid", atlas: 0, x: 381, y: 281, width: 102, height: 24 }),
-      GoodBoy: tx({ id: "Font.GoodBoy", atlas: 0, x: 501, y: 209, width: 256, height: 128 }),
-      OldMaiden: tx({ id: "Font.OldMaiden", atlas: 0, x: 501, y: 0, width: 308, height: 208 }),
+      Diggit: tx({ id: "Font.Diggit", atlas: 0, x: 810, y: 352, width: 128, height: 8 }),
+      ErotixLight: tx({ id: "Font.ErotixLight", atlas: 0, x: 810, y: 231, width: 160, height: 34 }),
+      Erotix: tx({ id: "Font.Erotix", atlas: 0, x: 810, y: 266, width: 160, height: 34 }),
+      Flaccid: tx({ id: "Font.Flaccid", atlas: 0, x: 381, y: 288, width: 102, height: 24 }),
+      GoodBoy: tx({ id: "Font.GoodBoy", atlas: 0, x: 0, y: 368, width: 256, height: 128 }),
+      OldMaiden: tx({ id: "Font.OldMaiden", atlas: 0, x: 501, y: 227, width: 308, height: 208 }),
     },
     Fx: {
-      Displacement: tx({ id: "Fx.Displacement", atlas: 0, x: 758, y: 209, width: 256, height: 256 }),
+      Displacement: tx({ id: "Fx.Displacement", atlas: 0, x: 0, y: 497, width: 256, height: 256 }),
     },
     Item: {
-      Box: tx({ id: "Item.Box", atlas: 0, x: 810, y: 130, width: 124, height: 56 }),
-      CigaretteLit: tx({ id: "Item.CigaretteLit", atlas: 0, x: 810, y: 198, width: 66, height: 10 }),
-      Cigarette: tx({ id: "Item.Cigarette", atlas: 0, x: 810, y: 187, width: 68, height: 10 }),
-      EggBroken: tx({ id: "Item.EggBroken", atlas: 0, x: 967, y: 70, width: 42, height: 54 }),
-      Egg: tx({ id: "Item.Egg", atlas: 0, x: 436, y: 357, width: 40, height: 46 }),
-      Flour: tx({ id: "Item.Flour", atlas: 0, x: 381, y: 306, width: 54, height: 64 }),
-      HalfCupFlour: tx({ id: "Item.HalfCupFlour", atlas: 0, x: 624, y: 338, width: 38, height: 36 }),
-      HalfCup: tx({ id: "Item.HalfCup", atlas: 0, x: 663, y: 338, width: 38, height: 36 }),
-      Lighter: tx({ id: "Item.Lighter", atlas: 0, x: 702, y: 338, width: 18, height: 36 }),
-      MixingBowl: tx({ id: "Item.MixingBowl", atlas: 0, x: 935, y: 155, width: 84, height: 46 }),
-      Potato: tx({ id: "Item.Potato", atlas: 0, x: 810, y: 70, width: 156, height: 50 }),
-      ScooperWithLatke: tx({ id: "Item.ScooperWithLatke", atlas: 0, x: 526, y: 338, width: 48, height: 42 }),
-      Scooper: tx({ id: "Item.Scooper", atlas: 0, x: 575, y: 338, width: 48, height: 42 }),
-      Skillet: tx({ id: "Item.Skillet", atlas: 0, x: 935, y: 130, width: 86, height: 24 }),
-      Smoke: tx({ id: "Item.Smoke", atlas: 0, x: 879, y: 187, width: 10, height: 20 }),
-      WhiskyGlass: tx({ id: "Item.WhiskyGlass", atlas: 0, x: 971, y: 0, width: 46, height: 60 }),
+      Box: tx({ id: "Item.Box", atlas: 0, x: 810, y: 361, width: 124, height: 56 }),
+      CigaretteLit: tx({ id: "Item.CigaretteLit", atlas: 0, x: 381, y: 324, width: 66, height: 10 }),
+      Cigarette: tx({ id: "Item.Cigarette", atlas: 0, x: 381, y: 313, width: 68, height: 10 }),
+      EggBroken: tx({ id: "Item.EggBroken", atlas: 0, x: 967, y: 301, width: 42, height: 54 }),
+      Egg: tx({ id: "Item.Egg", atlas: 0, x: 355, y: 400, width: 40, height: 46 }),
+      Flour: tx({ id: "Item.Flour", atlas: 0, x: 381, y: 335, width: 54, height: 64 }),
+      HalfCupFlour: tx({ id: "Item.HalfCupFlour", atlas: 0, x: 396, y: 400, width: 38, height: 36 }),
+      HalfCup: tx({ id: "Item.HalfCup", atlas: 0, x: 435, y: 425, width: 38, height: 36 }),
+      Lighter: tx({ id: "Item.Lighter", atlas: 0, x: 481, y: 374, width: 18, height: 36 }),
+      MixingBowl: tx({ id: "Item.MixingBowl", atlas: 0, x: 935, y: 386, width: 84, height: 46 }),
+      Potato: tx({ id: "Item.Potato", atlas: 0, x: 810, y: 301, width: 156, height: 50 }),
+      ScooperWithLatke: tx({ id: "Item.ScooperWithLatke", atlas: 0, x: 257, y: 368, width: 48, height: 42 }),
+      Scooper: tx({ id: "Item.Scooper", atlas: 0, x: 306, y: 368, width: 48, height: 42 }),
+      Skillet: tx({ id: "Item.Skillet", atlas: 0, x: 935, y: 361, width: 86, height: 24 }),
+      Smoke: tx({ id: "Item.Smoke", atlas: 0, x: 1013, y: 0, width: 10, height: 20 }),
+      WhiskyGlass: tx({ id: "Item.WhiskyGlass", atlas: 0, x: 450, y: 313, width: 46, height: 60 }),
     },
     Ui: {
       Error: tx({ id: "Ui.Error", atlas: 0, x: 0, y: 0, width: 500, height: 280 }),
-      Target: tx({ id: "Ui.Target", atlas: 0, x: 721, y: 338, width: 22, height: 34 }),
+      Target: tx({ id: "Ui.Target", atlas: 0, x: 474, y: 425, width: 22, height: 34 }),
+    },
+    World: {
+      RecipeBookClosed: tx({ id: "World.RecipeBookClosed", atlas: 0, x: 834, y: 0, width: 178, height: 230 }),
+      RecipeBookOpened: tx({ id: "World.RecipeBookOpened", atlas: 0, x: 501, y: 0, width: 332, height: 226 }),
+      RecipeStepComplete0: tx({ id: "World.RecipeStepComplete0", atlas: 0, x: 381, y: 281, width: 112, height: 6 }),
     },
   };
 }
--- src/igua/scenes/scn-main.ts
+++ src/igua/scenes/scn-main.ts
@@ -1,6 +1,8 @@
+import { DisplayObject } from "pixi.js";
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
 import { Mzk } from "../../assets/music";
 import { Jukebox } from "../core/igua-audio";
+import { mxnInteractive } from "../mixins/mxn-interactive";
 import { mxnMishaControlled, objMisha } from "../objects/obj-misha";
 
 export function scnMain() {
@@ -12,4 +14,32 @@ export function scnMain() {
         .at(250, 140)
         .zIndexed(999)
         .show();
+
+    {
+        function mxnRecipeStep(obj: DisplayObject) {
+            return obj
+                .step(() => obj.visible = lvl.RecipeBookOpened.visible);
+        }
+
+        lvl.RecipeBookOpened.step(self => self.visible = lvl.RecipeBook.destroyed);
+        lvl.RecipeBook
+            .mixin(mxnInteractive, {
+                text: "Open Recipe Book",
+                interact: () => {
+                    lvl.RecipeBook.destroy();
+                },
+            });
+
+        lvl.StepPotatoGroup.mixin(mxnRecipeStep);
+        lvl.StepCarrotGroup.mixin(mxnRecipeStep);
+        lvl.StepOnionGroup.mixin(mxnRecipeStep);
+        lvl.StepGarlicGroup.mixin(mxnRecipeStep);
+        lvl.StepSaltGroup.mixin(mxnRecipeStep);
+        lvl.StepPepperGroup.mixin(mxnRecipeStep);
+        lvl.StepFlourGroup.mixin(mxnRecipeStep);
+        lvl.StepCombinedGroup.mixin(mxnRecipeStep);
+        lvl.StepOliveOilGroup.mixin(mxnRecipeStep);
+        lvl.StepScoopedGroup.mixin(mxnRecipeStep);
+        lvl.StepServedGroup.mixin(mxnRecipeStep);
+    }
 }
