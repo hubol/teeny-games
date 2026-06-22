5419e4bff530c90dc6ece5cbc0f30c62e7827534
Hubol <hubol.gordon@gmail.com>
2026-06-22 15:12:28 -0500

    Toppings are announced on selection

--- src/igua/objects/obj-topping-container.ts
+++ src/igua/objects/obj-topping-container.ts
@@ -1,5 +1,8 @@
-import { Sprite } from "pixi.js";
+import { DisplayObject, Sprite } from "pixi.js";
+import { Sfx } from "../../assets/sounds";
 import { Tx } from "../../assets/textures";
+import { SceneLocal } from "../../lib/game-engine/scene-local";
+import { Null } from "../../lib/types/null";
 import { DataToppings } from "../data/data-toppings";
 import { PizzaTopping } from "../data/pizza-topping";
 import { mxnFxFlipH } from "../mixins/fx/mxn-fx-flip-h";
@@ -12,7 +15,17 @@ export function objToppingContainer(toppingId: DataToppings.Id) {
         .step(self => {
             const pointer = PizzaPointer.claim(self);
             if (pointer) {
+                if (CtxLastToppingContainer.value.obj !== self) {
+                    const sound = Sfx.Dialog.Toppings[toppingId];
+                    sound?.rate(0.9, 1.1)?.play();
+                    CtxLastToppingContainer.value.obj = self;
+                }
                 objTopping(PizzaTopping.create(toppingId), pointer).show();
             }
         });
 }
+
+const CtxLastToppingContainer = new SceneLocal(
+    () => ({ obj: Null<DisplayObject>() }),
+    "CtxLastToppingContainer",
+);
