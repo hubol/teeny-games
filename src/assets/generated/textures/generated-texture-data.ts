ec3ee22cf4db8278a084f8bb1c941968d61639d0
hubol <hubol.gordon@gmail.com>
2026-07-01 22:19:36 -0500

    Topping containers get happy

--- src/assets/generated/textures/generated-texture-data.ts
+++ src/assets/generated/textures/generated-texture-data.ts
@@ -1,6 +1,6 @@
 // This file is generated
 
-const atlases = [{ url: require("./atlas0.png"), texturesCount: 39 }];
+const atlases = [{ url: require("./atlas0.png"), texturesCount: 43 }];
 
 interface TxData {
   id: string;
@@ -18,23 +18,29 @@ function txs<T>(tx: (data: TxData) => T) {
       Tuna: tx({ id: "Characters.Tuna", atlas: 0, x: 0, y: 1201, width: 1024, height: 508 }),
     },
     Containers: {
-      GreenPepper: tx({ id: "Containers.GreenPepper", atlas: 0, x: 1298, y: 1480, width: 224, height: 126 }),
-      Mushroom: tx({ id: "Containers.Mushroom", atlas: 0, x: 3795, y: 0, width: 292, height: 108 }),
-      Onion: tx({ id: "Containers.Onion", atlas: 0, x: 1025, y: 1480, width: 272, height: 104 }),
+      GreenPepper: tx({ id: "Containers.GreenPepper", atlas: 0, x: 1795, y: 1402, width: 224, height: 126 }),
+      Happy: {
+        GreenPepper: tx({ id: "Containers.Happy.GreenPepper", atlas: 0, x: 1282, y: 1480, width: 224, height: 126 }),
+        Mushroom: tx({ id: "Containers.Happy.Mushroom", atlas: 0, x: 3795, y: 0, width: 292, height: 108 }),
+        Onion: tx({ id: "Containers.Happy.Onion", atlas: 0, x: 2308, y: 1232, width: 272, height: 104 }),
+        Tomato: tx({ id: "Containers.Happy.Tomato", atlas: 0, x: 3250, y: 57, width: 288, height: 90 }),
+      },
+      Mushroom: tx({ id: "Containers.Mushroom", atlas: 0, x: 3747, y: 1546, width: 292, height: 108 }),
+      Onion: tx({ id: "Containers.Onion", atlas: 0, x: 2581, y: 1232, width: 272, height: 104 }),
       Pepperoni: tx({ id: "Containers.Pepperoni", atlas: 0, x: 3747, y: 1187, width: 336, height: 149 }),
-      Tomato: tx({ id: "Containers.Tomato", atlas: 0, x: 3250, y: 57, width: 288, height: 90 }),
+      Tomato: tx({ id: "Containers.Tomato", atlas: 0, x: 3747, y: 1655, width: 288, height: 90 }),
     },
     Effects: {
       Bubble68: tx({ id: "Effects.Bubble68", atlas: 0, x: 3250, y: 0, width: 544, height: 56 }),
-      Shadow256: tx({ id: "Effects.Shadow256", atlas: 0, x: 1025, y: 1585, width: 256, height: 256 }),
+      Shadow256: tx({ id: "Effects.Shadow256", atlas: 0, x: 2854, y: 1232, width: 256, height: 256 }),
     },
     Faces: {
       Idle: tx({ id: "Faces.Idle", atlas: 0, x: 2113, y: 0, width: 1136, height: 150 }),
       Pepperoni: tx({ id: "Faces.Pepperoni", atlas: 0, x: 3539, y: 57, width: 128, height: 30 }),
       Pixel: {
-        GreenPepper: tx({ id: "Faces.Pixel.GreenPepper", atlas: 0, x: 1795, y: 1402, width: 108, height: 52 }),
-        Mushroom: tx({ id: "Faces.Pixel.Mushroom", atlas: 0, x: 1904, y: 1402, width: 100, height: 42 }),
-        Onion: tx({ id: "Faces.Pixel.Onion", atlas: 0, x: 1904, y: 1445, width: 100, height: 50 }),
+        GreenPepper: tx({ id: "Faces.Pixel.GreenPepper", atlas: 0, x: 1282, y: 1607, width: 108, height: 52 }),
+        Mushroom: tx({ id: "Faces.Pixel.Mushroom", atlas: 0, x: 1391, y: 1607, width: 100, height: 42 }),
+        Onion: tx({ id: "Faces.Pixel.Onion", atlas: 0, x: 1391, y: 1650, width: 100, height: 50 }),
         Tomato: tx({ id: "Faces.Pixel.Tomato", atlas: 0, x: 3861, y: 109, width: 144, height: 34 }),
       },
       Sing: tx({ id: "Faces.Sing", atlas: 0, x: 3194, y: 1036, width: 852, height: 150 }),
@@ -44,7 +50,7 @@ function txs<T>(tx: (data: TxData) => T) {
       ErotixLight: tx({ id: "Font.ErotixLight", atlas: 0, x: 3539, y: 109, width: 160, height: 34 }),
       Erotix: tx({ id: "Font.Erotix", atlas: 0, x: 3700, y: 109, width: 160, height: 34 }),
       Flaccid: tx({ id: "Font.Flaccid", atlas: 0, x: 3668, y: 57, width: 102, height: 24 }),
-      GoodBoy: tx({ id: "Font.GoodBoy", atlas: 0, x: 2308, y: 1232, width: 256, height: 128 }),
+      GoodBoy: tx({ id: "Font.GoodBoy", atlas: 0, x: 2308, y: 1337, width: 256, height: 128 }),
       OldMaiden: tx({ id: "Font.OldMaiden", atlas: 0, x: 3747, y: 1337, width: 308, height: 208 }),
     },
     Pizza: {
@@ -53,21 +59,21 @@ function txs<T>(tx: (data: TxData) => T) {
       Shading: tx({ id: "Pizza.Shading", atlas: 0, x: 2113, y: 151, width: 1080, height: 1080 }),
     },
     Shadows: {
-      Tomato: tx({ id: "Shadows.Tomato", atlas: 0, x: 4004, y: 1631, width: 62, height: 46 }),
+      Tomato: tx({ id: "Shadows.Tomato", atlas: 0, x: 3111, y: 1232, width: 62, height: 46 }),
     },
     Toppings: {
-      Beef: tx({ id: "Toppings.Beef", atlas: 0, x: 4004, y: 1678, width: 60, height: 60 }),
+      Beef: tx({ id: "Toppings.Beef", atlas: 0, x: 4036, y: 1655, width: 60, height: 60 }),
       Onion: tx({ id: "Toppings.Onion", atlas: 0, x: 1025, y: 1201, width: 512, height: 278 }),
-      Pepperoni0: tx({ id: "Toppings.Pepperoni0", atlas: 0, x: 3747, y: 1546, width: 256, height: 280 }),
-      Pepperoni1: tx({ id: "Toppings.Pepperoni1", atlas: 0, x: 2051, y: 1232, width: 256, height: 280 }),
-      Pepperoni2: tx({ id: "Toppings.Pepperoni2", atlas: 0, x: 1538, y: 1402, width: 256, height: 280 }),
-      Pepperoni: tx({ id: "Toppings.Pepperoni", atlas: 0, x: 4004, y: 1546, width: 86, height: 84 }),
-      Pineapple0: tx({ id: "Toppings.Pineapple0", atlas: 0, x: 2565, y: 1232, width: 256, height: 200 }),
-      Pineapple1: tx({ id: "Toppings.Pineapple1", atlas: 0, x: 2308, y: 1361, width: 256, height: 180 }),
+      Pepperoni0: tx({ id: "Toppings.Pepperoni0", atlas: 0, x: 2051, y: 1232, width: 256, height: 280 }),
+      Pepperoni1: tx({ id: "Toppings.Pepperoni1", atlas: 0, x: 1538, y: 1402, width: 256, height: 280 }),
+      Pepperoni2: tx({ id: "Toppings.Pepperoni2", atlas: 0, x: 1025, y: 1480, width: 256, height: 280 }),
+      Pepperoni: tx({ id: "Toppings.Pepperoni", atlas: 0, x: 1282, y: 1660, width: 86, height: 84 }),
+      Pineapple0: tx({ id: "Toppings.Pineapple0", atlas: 0, x: 2565, y: 1337, width: 256, height: 200 }),
+      Pineapple1: tx({ id: "Toppings.Pineapple1", atlas: 0, x: 2308, y: 1466, width: 256, height: 180 }),
       Pixel: {
-        GreenPepper: tx({ id: "Toppings.Pixel.GreenPepper", atlas: 0, x: 4004, y: 1739, width: 54, height: 52 }),
-        Mushroom: tx({ id: "Toppings.Pixel.Mushroom", atlas: 0, x: 4004, y: 1792, width: 50, height: 42 }),
-        Onion: tx({ id: "Toppings.Pixel.Onion", atlas: 0, x: 1795, y: 1455, width: 50, height: 50 }),
+        GreenPepper: tx({ id: "Toppings.Pixel.GreenPepper", atlas: 0, x: 4040, y: 1546, width: 54, height: 52 }),
+        Mushroom: tx({ id: "Toppings.Pixel.Mushroom", atlas: 0, x: 4040, y: 1599, width: 50, height: 42 }),
+        Onion: tx({ id: "Toppings.Pixel.Onion", atlas: 0, x: 4036, y: 1716, width: 50, height: 50 }),
         Tomato: tx({ id: "Toppings.Pixel.Tomato", atlas: 0, x: 4047, y: 1036, width: 48, height: 34 }),
       },
       Tomato: tx({ id: "Toppings.Tomato", atlas: 0, x: 1538, y: 1201, width: 512, height: 200 }),
--- src/igua/objects/obj-topping-container.ts
+++ src/igua/objects/obj-topping-container.ts
@@ -2,6 +2,7 @@ import { DisplayObject, Sprite } from "pixi.js";
 import { Sfx } from "../../assets/sounds";
 import { Tx } from "../../assets/textures";
 import { SceneLocal } from "../../lib/game-engine/scene-local";
+import { vnew } from "../../lib/math/vector-type";
 import { container } from "../../lib/pixi/container";
 import { Null } from "../../lib/types/null";
 import { DataToppings } from "../data/data-toppings";
@@ -12,10 +13,16 @@ import { PizzaPointer } from "../utils/pizza-pointer";
 import { objTopping } from "./obj-topping";
 
 export function objToppingContainer(toppingId: DataToppings.Id) {
-    return objFigureToppingContainer(toppingId)
+    const speed = vnew();
+    const figureObj = objFigureToppingContainer(toppingId);
+    return container(figureObj)
         .step(self => {
             const pointer = PizzaPointer.claim(self);
             if (pointer) {
+                if (speed.y >= 0) {
+                    speed.y = -4;
+                }
+                figureObj.objFigureToppingContainer.happyStepsCount = 10;
                 if (CtxLastToppingContainer.value.obj !== self) {
                     const sound = Sfx.Dialog.Toppings[toppingId];
                     sound?.rate(0.9, 1.1)?.play();
@@ -23,22 +30,48 @@ export function objToppingContainer(toppingId: DataToppings.Id) {
                 }
                 objTopping(PizzaTopping.create(toppingId), pointer).show();
             }
+
+            self.pivot.add(speed, -1);
+            if (self.pivot.y <= 0) {
+                self.pivot.y = 0;
+                speed.y = 0;
+            }
+            else {
+                speed.add(0, 0.4);
+            }
         });
 }
 
 function objFigureToppingContainer(toppingId: DataToppings.Id) {
-    if (toppingId in Tx.Containers) {
-        const txs = Tx.Containers[toppingId].split({ count: 2 });
+    const api = {
+        happyStepsCount: 0,
+    };
 
-        return container(
-            Sprite.from(txs[0]),
-            Sprite.from(txs[1]).mixin(mxnFxBoil, "position"),
+    const obj = container()
+        .merge({ objFigureToppingContainer: api })
+        .step(() => api.happyStepsCount--);
+
+    if (toppingId in Tx.Containers && toppingId in Tx.Containers.Happy) {
+        const txs = Tx.Containers[toppingId].split({ count: 2 });
+        const happyTxs = Tx.Containers.Happy[toppingId].split({ count: 2 });
+
+        container(
+            Sprite.from(txs[0])
+                .step(self => self.texture = api.happyStepsCount <= 0 ? txs[0] : happyTxs[0]),
+            Sprite.from(txs[1])
+                .mixin(mxnFxBoil, "position")
+                .step(self => self.texture = api.happyStepsCount <= 0 ? txs[1] : happyTxs[1]),
         )
-            .scaled(2, 2);
+            .scaled(2, 2)
+            .show(obj);
+    }
+    else {
+        Sprite.from(Tx.Containers.Pepperoni)
+            .mixin(mxnFxFlipH)
+            .show(obj);
     }
 
-    return Sprite.from(Tx.Containers.Pepperoni)
-        .mixin(mxnFxFlipH);
+    return obj;
 }
 
 const CtxLastToppingContainer = new SceneLocal(
