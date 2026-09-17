88495f72c387f981e9349edde64c957df32ba959
Hubol <hubol.gordon@gmail.com>
2026-09-17 13:59:14 -0500

    Text

--- src/assets/generated/sounds/generated-sfx-data.ts
+++ src/assets/generated/sounds/generated-sfx-data.ts
@@ -1,9 +1,14 @@
 // This file is generated
 
 async function sfxs<T>(sfx: (ogg: string) => Promise<T>) {
-  const sounds = await Promise.all(["placeholder.ogg"].map(sfx));
+  const sounds = await Promise.all(
+    ["message.ogg", "placeholder.ogg", "stroke0.ogg", "stroke1.ogg"].map(sfx),
+  );
   return {
-    Placeholder: sounds[0],
+    Message: sounds[0],
+    Placeholder: sounds[1],
+    Stroke0: sounds[2],
+    Stroke1: sounds[3],
   };
 }
 
--- src/igua/scenes/scn-main.ts
+++ src/igua/scenes/scn-main.ts
@@ -1,16 +1,45 @@
 import { RAD_TO_DEG } from "pixi.js";
+import { objText } from "../../assets/fonts";
+import { Sfx } from "../../assets/sounds";
+import { sleep } from "../../lib/game-engine/routines/sleep";
+import { approachLinear } from "../../lib/math/number";
 import { Rng } from "../../lib/math/rng";
 import { vdir } from "../../lib/math/vector";
 import { vnew } from "../../lib/math/vector-type";
+import { renderer } from "../current-pixi-renderer";
 import { Key } from "../globals";
 import { objCharacterCole } from "../objects/character/obj-character-cole";
 
 export function scnMain() {
+    const textObj = objText.Large("Welcome to Age 30. ", { maxWidth: renderer.width })
+        .coro(function* (self) {
+            for (const message of ["Walk with ARROW KEYS. ", "Masturbate with HOLD SPACE. "]) {
+                yield sleep(2000);
+                Sfx.Message.play();
+                self.text += "\n" + message;
+            }
+        })
+        .step(self => {
+            const delta = self.height - renderer.height;
+            if (delta < 0) {
+                return;
+            }
+            self.y = approachLinear(self.y, -delta, 1);
+        })
+        .show();
+
     objCharacterCole()
-        .at(250, 140)
+        .at(250, 200)
         .step(self => {
             if (Key.isDown("Space") && Rng.float() < 0.1) {
-                self.objCharacterCole.strokesCount++;
+                const isStroke0 = self.objCharacterCole.strokesCount++ % 2 === 0;
+                self.play((isStroke0 ? Sfx.Stroke0 : Sfx.Stroke1).rate(0.95, 1.05));
+                if (Rng.float() < 0.02) {
+                    textObj.text += "You'll never get to cum. ";
+                }
+                else {
+                    textObj.text += Rng.choose("You stroke your dick. ", "You stroke your dick! ");
+                }
             }
 
             const v = vnew();
@@ -36,5 +65,6 @@ export function scnMain() {
             self.objCharacterCole.facingDegrees = vdir(v) * RAD_TO_DEG;
             self.objCharacterCole.pedometer += 1;
         })
-        .show();
+        .show()
+        .objCharacterCole.facingDegrees = 90;
 }
